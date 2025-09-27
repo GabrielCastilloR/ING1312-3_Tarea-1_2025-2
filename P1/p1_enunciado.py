889630f28663e14c0e9df733026bc4871ba2903a
Gabriel Castllo <gabrielcastillor@gmail.com>
2025-09-27 17:34:47 -0300

    Finished updating

--- P1/p1_enunciado.py
+++ P1/p1_enunciado.py
@@ -6,7 +6,7 @@
     Curso:			ING1312
     Seccion:		3
     Descripcion:    Código presentado en el enunciado del problema 1 de la tarea 1.
-    """
+
 A = 10
 B = 20
 C = "Hola"
@@ -14,3 +14,26 @@ D = 3.1415
 B = A
 Nombre = input("Ingresa tu nomre: ")
 Saludo = C + " " + Nombre + "! Cómo estás?"
+
+"""
+#P 1.2: Intercambio de variables
+C = "Hola"
+D = 3.1415
+Temp = None
+print("Previo al intercambio...")
+print("Valor C: ", C)
+print("Valor D: ", D)
+print("Valor Temp: ", Temp)
+print("-"*30)
+
+#Comienza el intercambio
+Temp = C
+C = D
+D = Temp
+Temp = None
+
+print("Posterior al intercambio...")
+print("Valor C: ", C)
+print("Valor D: ", D)
+print("Valor Temp: ", Temp)
+print("-"*30)
